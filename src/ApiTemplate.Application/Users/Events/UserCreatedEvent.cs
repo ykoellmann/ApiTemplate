d38bb3ad44c34f10f863de66332bf8ca04d3a807
yankoePC <yankoe@outlook.de>
2024-01-12 20:35:01 +0100

    f

--- src/ApiTemplate.Application/Users/Events/UserCreatedEvent.cs
+++
@@ -1,7 +0,0 @@
-﻿using ApiTemplate.Application.Common.Events;
-using ApiTemplate.Domain.Users;
-using ApiTemplate.Domain.Users.ValueObjects;
-
-namespace ApiTemplate.Application.Users.Events;
-
-public record UserCreatedEvent(User user) : ClearCacheEvent<User, UserId>(user);
